eff894baa397177cf356e0b9b70a93e4eb87c1a5
J2D2Development <joedriscoll79@gmail.com>
2016-09-09 16:07:46 -0700

    Pushing initial name calc version and basic angular setup

---
+++ app.js
@@ -0,0 +1,8 @@
+(function() {
+    'use strict';
+
+    angular.module('myApp', [])
+        .controller('MainController', function($scope) {
+            $scope.name = "Joseph";
+        });
+})();
