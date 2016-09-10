47f90abc5f7c39020a144cb1c34071bb126a2369
J2D2Development <joedriscoll79@gmail.com>
2016-09-10 08:39:29 -0700

    First pass- assignment one

--- app.js
+++ app.js
@@ -2,7 +2,29 @@
     'use strict';
 
     angular.module('myApp', [])
-        .controller('MainController', function($scope) {
-            $scope.name = "Joseph";
-        });
+        .controller('LunchCheckController', LunchCheckController);
+
+        LunchCheckController.$inject = ['$scope'];
+
+        function LunchCheckController($scope) {
+            $scope.lunchItems = '';
+            $scope.message = '';
+
+            $scope.checkLunchItems = function() {
+                var trimmed = $scope.lunchItems.trim();
+
+                if(trimmed.length === 0) {
+                    $scope.message = 'Please enter data first.';
+                } else if(trimmed.split(',').length <= 3) {
+                    $scope.message = 'Enjoy!';
+                } else {
+                    $scope.message = 'Too much!';
+                }
+            }
+
+            $scope.clearLunchItems = function() {
+                $scope.lunchItems = '';
+                $scope.message = '';
+            }
+        }
 })();
